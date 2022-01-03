495a4fdd80d83b2d42e6accd5e66ebc6210bde7b
saad17334 <96839952+saad17334@users.noreply.github.com>
2022-01-03 11:36:59 +0530

    Update tic tac toe.cpp

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -1,121 +1,295 @@
-#include <iostream>
-#include <stdlib.h>
-using namespace std;
-//Array for the board
-char board[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-//Variable Declaration
-int choice;
-int row,column;
-char turn = 'X';
-bool draw = false;
+#include<bits/stdc++.h> 
+using namespace std; 
 
-//Function to show the current status of the gaming board
+#define COMPUTER 1 
+#define HUMAN 2 
 
-void display_board(){
+#define SIDE 3 // Length of the board 
 
-    //Rander Game Board LAYOUT
+// Computer will move with 'O' 
+// and human with 'X' 
+#define COMPUTERMOVE 'O' 
+#define HUMANMOVE 'X' 
 
-    cout<<"PLAYER - 1 [X]t PLAYER - 2 [O]nn";
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[0][0]<<"  | "<<board[0][1]<<"  |  "<<board[0][2]<<" n";
-    cout<<"tt_____|_____|_____n";
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[1][0]<<"  | "<<board[1][1]<<"  |  "<<board[1][2]<<" n";
-    cout<<"tt_____|_____|_____n";
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[2][0]<<"  | "<<board[2][1]<<"  |  "<<board[2][2]<<" n";
-    cout<<"tt     |     |     n";
+// A function to show the current board status 
+void showBoard(char board[][SIDE]) 
+{ 
+	
+	printf("\t\t\t %c | %c | %c \n", board[0][0], board[0][1], board[0][2]); 
+	printf("\t\t\t-----------\n"); 
+	printf("\t\t\t %c | %c | %c \n", board[1][0], board[1][1], board[1][2]); 
+	printf("\t\t\t-----------\n"); 
+	printf("\t\t\t %c | %c | %c \n\n", board[2][0], board[2][1], board[2][2]);  
+} 
+
+// A function to show the instructions 
+void showInstructions() 
+{ 
+	printf("\nChoose a cell numbered from 1 to 9 as below and play\n\n"); 
+	
+	printf("\t\t\t 1 | 2 | 3 \n"); 
+	printf("\t\t\t-----------\n"); 
+	printf("\t\t\t 4 | 5 | 6 \n"); 
+	printf("\t\t\t-----------\n"); 
+	printf("\t\t\t 7 | 8 | 9 \n\n"); 
+} 
+
+
+// A function to initialise the game 
+void initialise(char board[][SIDE]) 
+{	
+	// Initially the board is empty 
+	for (int i=0; i<SIDE; i++) 
+	{ 
+		for (int j=0; j<SIDE; j++) 
+			board[i][j] = ' '; 
+	} 
+} 
+
+// A function to declare the winner of the game 
+void declareWinner(int whoseTurn) 
+{ 
+	if (whoseTurn == COMPUTER) 
+		printf("COMPUTER has won\n"); 
+	else
+		printf("HUMAN has won\n"); 
+} 
+
+// A function that returns true if any of the row 
+// is crossed with the same player's move 
+bool rowCrossed(char board[][SIDE]) 
+{ 
+	for (int i=0; i<SIDE; i++) 
+	{ 
+		if (board[i][0] == board[i][1] && 
+			board[i][1] == board[i][2] && 
+			board[i][0] != ' ') 
+			return (true); 
+	} 
+	return(false); 
+} 
+
+// A function that returns true if any of the column 
+// is crossed with the same player's move 
+bool columnCrossed(char board[][SIDE]) 
+{ 
+	for (int i=0; i<SIDE; i++) 
+	{ 
+		if (board[0][i] == board[1][i] && 
+			board[1][i] == board[2][i] && 
+			board[0][i] != ' ') 
+			return (true); 
+	} 
+	return(false); 
+} 
+
+// A function that returns true if any of the diagonal 
+// is crossed with the same player's move 
+bool diagonalCrossed(char board[][SIDE]) 
+{ 
+	if (board[0][0] == board[1][1] && 
+		board[1][1] == board[2][2] && 
+		board[0][0] != ' ') 
+		return(true); 
+		
+	if (board[0][2] == board[1][1] && 
+		board[1][1] == board[2][0] && 
+		board[0][2] != ' ') 
+		return(true); 
+
+	return(false); 
+} 
+
+// A function that returns true if the game is over 
+// else it returns a false 
+bool gameOver(char board[][SIDE]) 
+{ 
+	return(rowCrossed(board) || columnCrossed(board) || diagonalCrossed(board) ); 
 }
 
-
-
-void player_turn(){
-    if(turn == 'X'){
-        cout<<"ntPlayer - 1 [X] turn : ";
-    }
-    else if(turn == 'O'){
-        cout<<"ntPlayer - 2 [O] turn : ";
-    }
-    
-
-    cin>> choice;
-
-    
-
-    switch(choice){
-        case 1: row=0; column=0; break;
-        case 2: row=0; column=1; break;
-        case 3: row=0; column=2; break;
-        case 4: row=1; column=0; break;
-        case 5: row=1; column=1; break;
-        case 6: row=1; column=2; break;
-        case 7: row=2; column=0; break;
-        case 8: row=2; column=1; break;
-        case 9: row=2; column=2; break;
-        default:
-            cout<<"Invalid Move";
-    }
-
-    if(turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O'){
-        //updating the position for 'X' symbol if
-        //it is not already occupied
-        board[row][column] = 'X';
-        turn = 'O';
-    }else if(turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O'){
-        //updating the position for 'O' symbol if
-        //it is not already occupied
-        board[row][column] = 'O';
-        turn = 'X';
-    }else {
-        //if input position already filled
-        cout<<"Box already filled!n Please choose another!!nn";
-        player_turn();
-    }
-    /* Ends */
-    display_board();
-}
-
-//Function to get the game status e.g. GAME WON, GAME DRAW GAME IN CONTINUE MODE
-
-bool gameover(){
-    //checking the win for Simple Rows and Simple Column
-    for(int i=0; i<3; i++)
-    if(board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
-    return false;
-
-    //checking the win for both diagonal
-
-    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
-    return false;
-
-    //Checking the game is in continue mode or not
-    for(int i=0; i<3; i++)
-    for(int j=0; j<3; j++)
-    if(board[i][j] != 'X' && board[i][j] != 'O')
-    return true;
-
-    //Checking the if game already draw
-    draw = true;
-    return false;
-}
-
-//Program Main Method
-
-int main()
+// Function to calculate best score
+int minimax(char board[][SIDE], int depth, bool isAI)
 {
-    cout<<"tttT I C K -- T A C -- T O E -- G A M Ettt";
-    cout<<"nttttFOR 2 PLAYERSnttt";
-    while(gameover()){
-        display_board();
-        player_turn();
-        gameover();
-    }
-    if(turn == 'X' && draw == false){
-        cout<<"nnCongratulations!Player with 'X' has won the game";
-    }
-    else if(turn == 'O' && draw == false){
-        cout<<"nnCongratulations!Player with 'O' has won the game";
-    }
-    else
-    cout<<"nnGAME DRAW!!!nn";
+	int score = 0;
+	int bestScore = 0;
+	if (gameOver(board) == true)
+	{
+		if (isAI == true)
+			return -1;
+		if (isAI == false)
+			return +1;
+	}
+	else
+	{
+		if(depth < 9)
+		{
+			if(isAI == true)
+			{
+				bestScore = -999;
+				for(int i=0; i<SIDE; i++)
+				{
+					for(int j=0; j<SIDE; j++)
+					{
+						if (board[i][j] == ' ')
+						{
+							board[i][j] = COMPUTERMOVE;
+							score = minimax(board, depth + 1, false);
+							board[i][j] = ' ';
+							if(score > bestScore)
+							{
+								bestScore = score;
+							}
+						}
+					}
+				}
+				return bestScore;
+			}
+			else
+			{
+				bestScore = 999;
+				for (int i = 0; i < SIDE; i++)
+				{
+					for (int j = 0; j < SIDE; j++)
+					{
+						if (board[i][j] == ' ')
+						{
+							board[i][j] = HUMANMOVE;
+							score = minimax(board, depth + 1, true);
+							board[i][j] = ' ';
+							if (score < bestScore)
+							{
+								bestScore = score;
+							}
+						}
+					}
+				}
+				return bestScore;
+			}
+		}
+		else
+		{
+			return 0;
+		}
+	}
+}
+
+// Function to calculate best move
+int bestMove(char board[][SIDE], int moveIndex)
+{
+	int x = -1, y = -1;
+	int score = 0, bestScore = -999;
+	for (int i = 0; i < SIDE; i++)
+	{
+		for (int j = 0; j < SIDE; j++)
+		{
+			if (board[i][j] == ' ')
+			{
+				board[i][j] = COMPUTERMOVE;
+				score = minimax(board, moveIndex+1, false);
+				board[i][j] = ' ';
+				if(score > bestScore)
+				{
+					bestScore = score;
+					x = i;
+					y = j;
+				}
+			}
+		}
+	}
+	return x*3+y;
+}
+
+// A function to play Tic-Tac-Toe 
+void playTicTacToe(int whoseTurn) 
+{ 
+	char board[SIDE][SIDE]; 
+	int moveIndex = 0, x = 0, y = 0;
+
+	initialise(board);
+	showInstructions(); 
+	
+	// Keep playing till the game is over or it is a draw 
+	while (gameOver(board) == false && moveIndex != SIDE*SIDE) 
+	{ 
+		int n;
+		if (whoseTurn == COMPUTER) 
+		{
+			n = bestMove(board, moveIndex);
+			x = n / SIDE;
+			y = n % SIDE;
+			board[x][y] = COMPUTERMOVE; 
+			printf("COMPUTER has put a %c in cell %d\n\n", COMPUTERMOVE, n+1);
+			showBoard(board);
+			moveIndex ++; 
+			whoseTurn = HUMAN;
+		} 
+		
+		else if (whoseTurn == HUMAN) 
+		{
+			printf("You can insert in the following positions : ");
+			for(int i=0; i<SIDE; i++)
+				for (int j = 0; j < SIDE; j++)
+					if (board[i][j] == ' ')
+						printf("%d ", (i * 3 + j) + 1);
+			printf("\n\nEnter the position = ");
+			scanf("%d",&n);
+			n--;
+			x = n / SIDE;
+			y = n % SIDE; 
+			if(board[x][y] == ' ' && n<9 && n>=0)
+			{
+				board[x][y] = HUMANMOVE; 
+				printf ("\nHUMAN has put a %c in cell %d\n\n", HUMANMOVE, n+1); 
+				showBoard(board); 
+				moveIndex ++; 
+				whoseTurn = COMPUTER;
+			}
+			else if(board[x][y] != ' ' && n<9 && n>=0)
+			{
+				printf("\nPosition is occupied, select any one place from the available places\n\n");
+			}
+			else if(n<0 || n>8)
+			{
+				printf("Invalid position\n");
+			}
+		} 
+	} 
+
+	// If the game has drawn 
+	if (gameOver(board) == false && moveIndex == SIDE * SIDE) 
+		printf("It's a draw\n"); 
+	else
+	{ 
+		// Toggling the user to declare the actual winner 
+		if (whoseTurn == COMPUTER) 
+			whoseTurn = HUMAN; 
+		else if (whoseTurn == HUMAN) 
+			whoseTurn = COMPUTER; 
+		
+		declareWinner(whoseTurn); 
+	} 
+} 
+
+int main() 
+{ 
+	printf("\n-------------------------------------------------------------------\n\n");
+	printf("\t\t\t Tic-Tac-Toe\n"); 
+	printf("\n-------------------------------------------------------------------\n\n");
+	char cont='y';
+	do {
+		char choice;
+	 	printf("Do you want to start first?(y/n) : ");
+	 	scanf(" %c", &choice);
+
+		if(choice=='n')
+			playTicTacToe(COMPUTER);
+		else if(choice=='y')
+			playTicTacToe(HUMAN);
+		else
+			printf("Invalid choice\n"); 
+        
+		printf("\nDo you want to quit(y/n) : ");
+       		scanf(" %c", &cont);
+	} while(cont=='n');
+	return (0); 
 } 
